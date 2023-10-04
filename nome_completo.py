62dd2141ad58d54281cfc6381aa4430c85e0a4bc
mari <marimmab@gmail.com>
2023-10-04 08:54:35 -0300

    Desenvolvimento da class nome completo

---
+++ nome_completo.py
@@ -0,0 +1,5 @@
+def print_full_name(first, last):
+    print("Hello " + first + " " + last + "! You just delved into python.")
+primeiro_nome = input()
+sobrenome = input()
+print_full_name(primeiro_nome, sobrenome)
